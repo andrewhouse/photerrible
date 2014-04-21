599ae381e653e2635a53e6b4af968cc197b8ba23
Andrew House <andrewhouse621@gmail.com>
2014-04-21 14:50:40 -0400

    trying to configure upload

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,7 +4,7 @@ class PicturesController < ApplicationController
   # GET /pictures
   # GET /pictures.json
   def index
-@pictures = Picture.all   
+    @pictures = Picture.all   
   end
 
   # GET /pictures/1
